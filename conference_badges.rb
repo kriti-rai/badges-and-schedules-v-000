c3805b4c698708ebe0f60e02b8fcbcebbcb88dcc
Kriti Rai <kriti.rai@gmail.com>
2018-01-03 01:42:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,4 @@
 def badge_maker(name)
-<<<<<<< HEAD
   "Hello, my name is #{name}."
 end
 array =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
@@ -7,13 +6,11 @@ def batch_badge_creator(array)
   badge_names=[]
   array.each {|i| badge_names<<badge_maker(i)}
   badge_names
-=======
   puts "Hello, my name is #{name}."
 end
 array =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(array)
   array.each {|i| puts badge_maker(i)}
->>>>>>> 415084470d8d2afd5bee2319d5366fff75d09536
 end
 
 def each_with_method(array)
@@ -21,18 +18,16 @@ def each_with_method(array)
 end
 
 def assign_rooms(array)
-<<<<<<< HEAD
-  room_assignements=[]
+  room_assignments=[]
   array.each_with_index do |a,i|
-    room_assignements<<"Hello, #{a}! You'll be assigned to room #{i+1}!"
+    room_assignments<<"Hello, #{a}! You'll be assigned to room #{i+1}!"
   end
-    room_assignements
+    room_assignments
 end
 
 def printer(array)
 	batch_badge_creator(array).each {|i| puts i}
   assign_rooms(array).each {|i| puts i}
-=======
   rooms[]
   array.each_with_index do |a,i|
     rooms<<"Hello #{a}! You will be assigned to room #{i+1}!"
@@ -41,6 +36,6 @@ def printer(array)
 end
 
 def printer(array)
-	batch_badge_creator(array).each{|i| puts i}
+	batch_badge_creator(array).each {|i| puts i}
 	assign_rooms(array).each {|i| puts i}
 end
